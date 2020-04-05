62573ee4fbaaae7b80ca6f5d772fcd7cfca6e58e
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-04-05 21:13:01 +0300

    init

--- MicrosoftTeamsStyleCalling/ContentView.swift
+++ MicrosoftTeamsStyleCalling/ContentView.swift
@@ -9,8 +9,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var a = false
+    @State private var b = false
+    @State private var c = false
+    @State private var d = false
+    @State private var e = false
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            RadialGradient(gradient: Gradient(colors: [Color.black, Color.gray]), center: .center, startRadius: 5, endRadius: 500)
+                .edgesIgnoringSafeArea(.all)
+            
+            Circle()
+                .foregroundColor(Color.black.opacity(0.2))
+                .frame(width: 350, height: 350)
+                .scaleEffect(a ? 1.2 : 0.5, anchor: .center)
+                .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true).delay(0.1))
+                .onAppear() {
+                    self.a.toggle()
+            }
+            
+            Circle()
+                .foregroundColor(Color.black.opacity(0.4))
+                .frame(width: 310, height: 310)
+                .scaleEffect(b ? 1.2 : 0.5, anchor: .center)
+                .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true).delay(0.2))
+                .onAppear() {
+                    self.b.toggle()
+            }
+            
+            Circle()
+                .foregroundColor(Color.black.opacity(0.6))
+                .frame(width: 270, height: 270)
+                .scaleEffect(c ? 1.2 : 0.5, anchor: .center)
+                .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true).delay(0.3))
+                .onAppear() {
+                    self.c.toggle()
+            }
+            
+            Image("girl")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 230, height: 230)
+                .clipShape(Circle())
+                .scaleEffect(d ? 1 : 1.4, anchor: .center)
+                .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true).delay(0.4))
+                .onAppear() {
+                    self.d.toggle()
+            }
+            
+            Text("Clarissa is calling")
+                .foregroundColor(.white)
+                .font(.headline)
+                .offset(y: 100)
+                .scaleEffect(d ? 1.1 : 0.95, anchor: .center)
+                .animation(Animation.easeInOut(duration: 1).repeatForever(autoreverses: true).delay(0.6))
+                    .onAppear() {
+                        self.e.toggle()
+                }
+        }
     }
 }
 
