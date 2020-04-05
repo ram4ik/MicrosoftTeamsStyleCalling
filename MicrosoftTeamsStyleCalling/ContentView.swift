b3fe5d220a1e5367ff7dd29ffba67c13bcbc8393
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-04-05 20:50:40 +0300

    Initial Commit

---
+++ MicrosoftTeamsStyleCalling/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MicrosoftTeamsStyleCalling
+//
+//  Created by Ramill Ibragimov on 05.04.2020.
+//  Copyright © 2020 Ramill Ibragimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
